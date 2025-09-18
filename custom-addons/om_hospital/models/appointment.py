924ea9c6445d21ca699f163964e961c1785e0f01
Ferko <roland.adam@hotmail.com>
2025-09-18 05:38:25 +0200

    Module 5: Fields Deep Dive : Define related and computed fields

--- custom-addons/om_hospital/models/appointment.py
+++ custom-addons/om_hospital/models/appointment.py
@@ -12,5 +12,6 @@ class HospitalAppointment(models.Model):
         comodel_name="hospital.patient", # Related model
         string="Patient", # Field label
     )
-    appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
-    booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
+    gender = fields.Selection(related='patient_id.gender') # Related field to patient
+    appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now) # Datetime field with default value
+    booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today) # Date field with default value
