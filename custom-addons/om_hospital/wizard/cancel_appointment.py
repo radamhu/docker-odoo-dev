3dd9938b4bde4138e72c5486103863f845001602
Ferko <roland.adam@hotmail.com>
2025-09-25 08:55:31 +0200

    Module 7: Workflows & Wizards Add text field in Wizard / Transient modul

--- custom-addons/om_hospital/wizard/cancel_appointment.py
+++ custom-addons/om_hospital/wizard/cancel_appointment.py
@@ -5,6 +5,7 @@ class CancelAppointmentWizard(models.TransientModel):
     _description = 'Cancel Appointment Wizard'
 
     appointment_id = fields.Many2one('hospital.appointment', string='Appointment', required=True)
+    reason = fields.Text(string='Reason for Cancellation')
     
     def action_cancel(self):
         return
