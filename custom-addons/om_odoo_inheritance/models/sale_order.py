621602fb3ace39246ab9249548a7091ca28f4093
Ferko <roland.adam@hotmail.com>
2025-09-26 14:49:48 +0200

    Module 8: Inheritance & ORM inherit functions

--- custom-addons/om_odoo_inheritance/models/sale_order.py
+++ custom-addons/om_odoo_inheritance/models/sale_order.py
@@ -4,3 +4,12 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     confirmed_user_id = fields.Many2one('res.users', string='Confirmed by')
+
+    # inherit an existing method and extend it
+    # in this case we extend action_confirm method from sale.order model
+    # to set the confirmed_user_id field to the current user
+    # when the sale order is confirmed
+    def action_confirm(self):
+        super(SaleOrder, self).action_confirm()
+        print("Custom action_confirm method called")
+        self.confirmed_user_id = self.env.user.id # set the current user as the confirmer, will give the current logged in user
