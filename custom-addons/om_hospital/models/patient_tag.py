b5b62dbdb9383ff6d6d31ac2f4d9e939c4d06c89
Ferko <roland.adam@hotmail.com>
2025-10-28 11:52:29 +0100

    Module 9: sql constraint

--- custom-addons/om_hospital/models/patient_tag.py
+++ custom-addons/om_hospital/models/patient_tag.py
@@ -8,3 +8,9 @@ class PatientTag(models.Model):
     active = fields.Boolean(string='Active', default=True)
     color = fields.Integer(string='Color Index', default=0)
     color_hex = fields.Char(string='Color Hex')
+    sequence = fields.Integer(string='Sequence')
+    
+    _sql_constraints = [
+        ('unique_tag_name', 'unique(name, active)', 'The tag name must be unique.'),
+        ('check_sequence', 'CHECK(sequence >= 0)', 'The sequence must be a non-negative integer.'),
+    ]
